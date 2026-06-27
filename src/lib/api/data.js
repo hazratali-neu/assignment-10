313624b38a6c8b141dbc31b773895066e9dcd351
hazrat <tora59459@gmail.com>
2026-06-27 15:14:29 +0600

    added my project ticket details page added

--- src/lib/api/data.js
+++ src/lib/api/data.js
@@ -1,18 +1,4 @@
 
-
-// export const getFetchAddticketData = async () => {
-//     const baseUrl = process.env.NEXT_PUBLIC_URL || 'http://localhost:8000';
-    
-//     const res = await fetch(`${baseUrl}/api/addticket`, {
-//         cache: 'no-store' 
-//     });
-
-//     if (!res.ok) {
-//         throw new Error('Failed to fetch addticket data');
-//     }
-//     return res.json();
-// };
-
 export const getFetchAddticketData = async (email) => {
   const url = email 
     ? `${process.env.NEXT_PUBLIC_URL}/api/addticket?email=${email}`
